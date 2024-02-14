d85e717583714bead2e1004fb08b21b25bd8ac2d
Jeric <jericjune.logan.lhoopa@gmail.com>
2024-02-14 16:57:00 +0800

    logout button

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+import { router } from "@inertiajs/vue3";
+import Button from "primevue/button";
+
+function logout() {
+    router.post(route("logout"));
+}
+</script>
+
 <template>
     <div>
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <header>
+                <div class="flex justify-end p-8">
+                    <Button
+                        label="Logout"
+                        icon="pi pi-sign-out"
+                        @click="logout()"
+                    />
+                </div>
+            </header>
+
             <main>
                 <slot />
             </main>
